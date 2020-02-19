99c90b4e4418737393620549eb563e0ea2274da4
Peter Jerman <peter.jerman@dhimahi.com>
2020-02-19 12:36:11 +0100

    Detached and renamed from fork source - update to Angular8 version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AngularCropperjsModule } from 'angular-cropperjs';
+import { AngularCropperjsModule } from 'ngx-cropper-js';
 
 @NgModule({
     declarations: [
