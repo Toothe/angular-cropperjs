463de7cc5106135c3c058fed65455dd496e5a7ec
Peter Jerman <peter.jerman@dhimahi.com>
2020-02-21 10:44:22 +0100

    Update public api

--- projects/ngx-cropper-js/src/public_api.ts
+++ projects/ngx-cropper-js/src/public_api.ts
@@ -1,7 +1,8 @@
 /*
  * Public API Surface of angular-cropperjs
  */
-
 export * from './lib/angular-cropperjs.service';
 export * from './lib/cropper/cropper.component';
+export * from './lib/cropper/cropper.enum';
+export * from './lib/cropper/cropper.interface';
 export * from './lib/angular-cropperjs.module';
